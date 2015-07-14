f3d107a797ace4d454ec363ed0283fb8288deb78
Jeremy Eaton <jeremyeaton89@gmail.com>
2015-07-14 15:34:30 -0700

    initial commit

---
+++ www/components/Feed.js
@@ -0,0 +1,12 @@
+/** @jsx React.DOM */
+var React = require('react/addons');
+
+var Feed = React.createClass({
+  render: function() {
+    return (
+      <h1>HI THERE!!!</h1>
+    );
+  }
+});
+
+module.exports = Feed;
---
+++ www/index.js
@@ -0,0 +1,53 @@
+/** @jsx React.DOM */
+
+var React              = require('react/addons');
+var CSSTransitionGroup = React.addons.CSSTransitionGroup;
+var Router             = require('react-router-component');
+var Location           = Router.Location;
+var Link               = Router.Link;
+var FastClick          = require('./lib/js/fastclick');
+
+var Feed               = require('./components/Feed');
+
+var AnimatedLocations = React.createClass({
+  mixins: [Router.RouterMixin, Router.AsyncRouteRenderingMixin],
+
+  getRoutes: function() {
+    return this.props.children;
+  },
+  render: function() {
+    var handler = this.renderRouteHandler();
+    var isPopState = this.state.navigation.isPopState;
+    var enabled = isPopState ?
+                  !!this.props.popStateTransitionName :
+                  !this.state.navigation.noTransition;
+    var props = {
+      component: React.DOM.div,
+      transitionEnter: enabled,
+      transitionLeave: enabled,
+    };
+    if (isPopState && this.props.popStateTransitionName) {
+      props.transitionName = this.props.popStateTransitionName;
+    } else if (this.state.navigation.transitionName) {
+      props.transitionName = this.state.navigation.transitionName;
+    }
+
+    handler.props.key = this.state.match.path;
+    return this.transferPropsTo(CSSTransitionGroup(props, handler));
+  }
+});
+
+var App = React.createClass({
+  render: function() {
+    return (
+      <AnimatedLocations hash className='main' transitionName='left'>
+        <Location path='/'                handler={Feed} />
+      </AnimatedLocations>
+    );
+  }
+});
+
+$(function() {
+  if (typeof FastClick == 'function') FastClick(document.body);
+  React.render(<App />, $('#container'));
+});
